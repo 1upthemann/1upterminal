896971520f876389cfa4d2914286378b8e319abc
1up <126313418+1upthemann@users.noreply.github.com>
2023-10-06 02:33:22 +0000

    Delete 1up terminal directory

--- 1up terminal/redirect.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("inputField").addEventListener("keyup", function(event) {
-    const sequence = "1up"; // The sequence of characters to trigger the redirection
-    const inputText = event.target.value;
-
-    if (inputText.endsWith(sequence)) {
-        window.location.href = "https://www.1upterminal.com/usernameandpassword.html"; // Replace with your desired link
-    }
-});
